1a1a9641bce5d1314b64cd631d19b7025a639a19
root <root@M4N14-Laptop.localdomain>
2021-12-03 02:21:06 -0800

    v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import React from 'react';
+import chaosStar from './chaos_star.svg';
 import './App.css';
+const crypto = require("crypto");
 
-function App() {
+const symbolMap = {
+  '0': '!',
+  '1': '@',
+  '2': '#',
+  '3': '$',
+  '4': '%',
+  '5': '^',
+  '6': '&',
+  '7': '*',
+  '8': '(',
+  '9': ')',
+  'a': '_',
+  'b': '+',
+  'c': '-',
+  'd': '=',
+  'e': '[',
+  'f': ']',
+}
+
+const randomBit = () => parseInt(crypto.randomBytes(1).toString('hex'), 16) > 127 ? 1 : 0;
+
+const generatePassword = () => {
+  let passwordBuild = crypto.randomBytes(10).toString('hex');
+  for (let i = 0; i < (passwordBuild.length*.25); i++) {
+    passwordBuild += symbolMap[passwordBuild[i]];
+  }
+
+  passwordBuild =  passwordBuild.split('').sort(() => 0.5 - randomBit()).join('');
+
+  return passwordBuild;
+}
+
+const App = () => {
+
+  let [generatedPassword, setGeneratedPassword] = React.useState('');
+  let [passwordTextClassName, setPasswordTextClassName] = React.useState("generated-password-text");
+
+  React.useEffect(() => {
+    setGeneratedPassword(generatePassword());
+  }, []);
+
+  React.useEffect(() => {
+    if (passwordTextClassName.includes('highlighter')) {
+      setTimeout(() => {
+        setPasswordTextClassName('generated-password-text');
+      }, 1000)
+    }
+  }, [passwordTextClassName]);
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(generatedPassword);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="header-text">
+        kaos.pw
+      </p>
+      <div className="chaos-star-container">
+        <img src={chaosStar} className="chaos-star" alt="chaos star" />
+      </div>
+      <p className={passwordTextClassName} onClick={() => {
+        copyToClipboard(generatedPassword);
+        setPasswordTextClassName(passwordTextClassName + ' highlighter');
+      }}>
+        { generatedPassword }
+      </p>
     </div>
   );
 }
